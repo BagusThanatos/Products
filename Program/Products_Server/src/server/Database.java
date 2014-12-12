df0fa6404949a4980a9bb9b207083019273862e0
BagusThanatos <bagilah@ymail.com>
2014-12-12 18:48:53 +0700

    added stub for database operation

--- Program/Products_Server/src/server/Database.java
+++ Program/Products_Server/src/server/Database.java
@@ -3,12 +3,55 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
 package server;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author BagusThanatos (github.com/BagusThanatos)
+ * @author BagusThanatos
  */
 public class Database {
+    private final String username= "root";
+    private final String pass="";
+    private Statement  stmt= null;
+    private Connection con= null;
+    private ResultSet rs= null;
     
+    public Database(){
+        /*try {
+            Class.forName("org.gjt.mm.mysql.Driver");
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }*/
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost/TPModul10",username,pass);
+            stmt= con.createStatement();
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+    }
+    
+    public ResultSet getData(String query){
+            try {
+                rs = stmt.executeQuery(query);
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+            return rs;
+    }
+    public void query(String query){
+        try {
+            stmt.executeUpdate(query);
+        } catch (SQLException ex) {
+            Logger.getLogger(Database.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 }
